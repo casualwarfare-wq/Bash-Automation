cda26c84882ffe4086b6e964d0f7bc84daa275a6
KING RAG <265420888+casualwarfare-wq@users.noreply.github.com>
2026-07-02 15:50:34 -0500

    extractor done

--- extractor.sh
+++ extractor.sh
@@ -2,7 +2,17 @@
 set -euo pipefail
 COM=$1
 COL=$2
-awk -F',' -v col="$COL" '{print $col}' "$COM"
+awk -F',' -v col="$COL" '{print $col}' "$COM" > extracted_column.txt
+echo "What do you want to count in the column?"
+read COUNT
+grep -n "$COUNT" extracted_column.txt > count_results.txt
+wc -l < count_results.txt
+
+
+
+
+
+
 
 
 
