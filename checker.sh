bd34d8173c17d22b761c67b8799a9067b1ac5e22
KING RAG <265420888+casualwarfare-wq@users.noreply.github.com>
2026-07-02 13:57:50 -0500

    checker done

--- checker.sh
+++ checker.sh
@@ -2,19 +2,23 @@
 DIR="./*"
 
 for file in $DIR; do
-    [ -e $file ] || continue
-    if [ -d $file ]; then
-        for subfile in $file/*; do
-            [ -e $subfile ] || continue
-            if [ -f $subfile ]; then
-                if [[ $subfile == pyvenv.cfg ]]; then
+    [ -e "$file" ] || continue
+    if [ -d "$file" ]; then
+        for subfile in "$file"/*; do
+            [ -f "$subfile" ] || continue
+            echo "Checking $subfile"
+                if [[ "$subfile" == "$file"/pyvenv.cfg ]]; then
                     echo "True: there is a python virtual environment in $DIR"
                     exit 0
                 fi
-            fi
         done
     fi
 done
 echo "False: there is no python virtual environment in $DIR"
+
+
+
+
+
 
 
