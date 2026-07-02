297a3a2382a83a3fa965791d424c5bebf15cdea2
KING RAG <265420888+casualwarfare-wq@users.noreply.github.com>
2026-07-02 11:13:05 -0500

    testing for files

--- filer.sh
+++ filer.sh
@@ -2,20 +2,24 @@
 if (($# >= 2))
 then
     PATH=$1
-    NAMES=$2
+    NAMES=(-a $2)
 elif (($# == 1))
 then
     PATH=$1
     echo "Enter an array of names separated by spaces:"
     read -a NAMES
+    echo "${NAMES[@]}"
 else
     echo "Usage: $0 <path> <name1 name2 ...>"
     exit 1
 fi
 count=0
 for file in $PATH; do
-    if [ -f $file ]; then
-        mv $file ${NAMES[$count]}
+    if [ -f "$file" ]; then
+        mv "$file" "${NAMES[$count]}"
+        echo "Renamed $file to ${NAMES[$count]}"
         ((count++))
     fi
 done
+
+
