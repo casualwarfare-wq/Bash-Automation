d0a050428e6db67f58bbf000fce07ce174efae86
KING RAG <265420888+casualwarfare-wq@users.noreply.github.com>
2026-07-03 10:12:10 -0500

    analysis fully done

--- analysis.py
+++ analysis.py
@@ -8,14 +8,15 @@ if len(sys.argv) != 2:
     sys.exit(1)
 
 input_path = sys.argv[1]
-output_dir = "OUTPUT"
-os.makedirs(output_dir, exist_ok=True)
 
 lot = pd.read_csv(input_path)
 lot = lot.ffill()
-output_path = os.path.join(output_dir, "lot.csv")
-lot.to_csv(output_path, index=False)
-print(f"Saved output to {output_path}")
+lot.to_csv("OUTPUT/lot.csv", index=False)
+
+
+
+
+
 
 
 
