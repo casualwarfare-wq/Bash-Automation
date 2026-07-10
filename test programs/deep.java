bb32a91335587a77ae21eaff91b306799097514d
KING RAG <265420888+casualwarfare-wq@users.noreply.github.com>
2026-07-10 18:27:47 -0500

    moved other files

--- test programs/deep.java
+++
@@ -1,5 +0,0 @@
-public class deep {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
--- test programs/forty.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-echo "This is a test script."
-VAR="hello"
-echo $VAR
-
-for ((i=10; i>0; i--))
- do
-    echo "$i"
-done
